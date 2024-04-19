3cd3f0369691ee51b275424ef447d677a6b845e1
Thomas Lu <89040679+thomaslu678@users.noreply.github.com>
2024-04-19 08:42:33 -0600

    04.19.2024

--- notes/src/org/canyonsdistrict/hhs/chapter17/BinarySearchTree.java
+++ notes/src/org/canyonsdistrict/hhs/chapter17/BinarySearchTree.java
@@ -1,18 +1,138 @@
 package org.canyonsdistrict.hhs.chapter17;
 
+import java.awt.geom.NoninvertibleTransformException;
+
 public class BinarySearchTree {
+
+    private Node root;
 
     public BinarySearchTree() {
     }
 
     public void add (Comparable obj) {
-
+        var newNode = new Node();
+        newNode.data = obj;
+        newNode.left = null;
+        newNode.right = null;
+        if (root == null) {
+            root = newNode;
+        }
+        else {
+            root.addNode(newNode);
+        }
     }
 
     class Node {
         public Comparable data;
         public Node left;
         public Node right;
+
+        public void addNode(Node newNode) {
+            var value = newNode.data.compareTo(data);
+            if (value < 0) {
+                if (left == null) {
+                    left = newNode;
+                }
+                else {
+                    left.addNode(newNode);
+                }
+            }
+            else if (value > 0) {
+                if (right == null) {
+                    right = newNode;
+                }
+                else {
+                    right.addNode(newNode);
+                }
+            }
+        }
+
+        public boolean find(Comparable obj) {
+            Node currentNode = root;
+
+            while (currentNode != null) {
+                var value = currentNode.data.compareTo(obj);
+                if (value == 0) {
+                    return true;
+                }
+                else if (value > 0) {
+                    currentNode = currentNode.left;
+                }
+                else {
+                    currentNode = currentNode.right;
+                }
+            }
+
+            return false;
+
+        }
+
+        public void remove(Comparable obj) {
+            Node nodeToRemove = root;
+            Node parent = null;
+            boolean found = false;
+
+            while (!found && nodeToRemove != null) {
+                var value = nodeToRemove.data.compareTo(obj);
+
+                if (value == 0) {
+                    found = true;
+                }
+                else {
+                    if (value > 0) {
+                        nodeToRemove = nodeToRemove.left;
+                    }
+                    else {
+                        nodeToRemove = nodeToRemove.right;
+                    }
+                }
+            }
+
+            if (!found) {
+                return;
+            }
+
+            if (nodeToRemove.left == null | nodeToRemove.right == null) {
+                Node newChild;
+                if (nodeToRemove.left == null) {
+                    newChild = nodeToRemove.right;
+                }
+                else {
+                    newChild = nodeToRemove.left;
+                }
+
+                if (parent == null) {
+                    root = newChild;
+                }
+                else if (parent.left == nodeToRemove) {
+                    parent.left = newChild;
+                }
+                else {
+                    parent.right = newChild;
+                }
+
+            }
+
+            Node smallestParent = nodeToRemove;
+            Node smallest = nodeToRemove.right;
+
+            if (smallest != null) {
+                while (smallest.left != null) {
+                    smallestParent = smallest;
+                    smallest = smallest.left;
+                }
+            }
+
+            nodeToRemove.data = smallest.data;
+            if (smallestParent == nodeToRemove) {
+                smallestParent.right = smallest.right;
+            }
+            else {
+                smallestParent.left = smallest.right;
+            }
+
+        }
+
     }
 
 }
---
+++ notes/src/org/canyonsdistrict/hhs/chapter17/BinarySearchTreeApp.java
@@ -0,0 +1,16 @@
+package org.canyonsdistrict.hhs.chapter17;
+
+public class BinarySearchTreeApp {
+
+    public static void main(String[] args) {
+
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.add(100);
+        tree.add(50);
+        tree.add(200);
+
+        System.out.println();
+
+    }
+
+}
